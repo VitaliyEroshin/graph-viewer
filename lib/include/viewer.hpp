037db01afc330e9089b491e27c645574c0dc02d6
vitaliy <vitaliy-eroshin@yandex.ru>
2022-02-28 01:11:19 +0300

    Renamed some class fields

--- lib/include/viewer.hpp
+++ lib/include/viewer.hpp
@@ -9,13 +9,20 @@ public:
   void addEdge(size_t parent, size_t son);   
   void show();
   void setRoot(size_t root);
-  
+
 private:
   struct Vertex {
-    size_t x;
+    size_t label;
     size_t level;
+    size_t size;
+
+    std::vector<Vertex> children;
+
+    Vertex(size_t label, size_t level, size_t size): label(label), level(level), size(size) {}
   };
 
-  size_t root = 0;
-  std::vector<std::vector<size_t>> graph; 
+  size_t _root = 0;
+  std::vector<std::vector<size_t>> _rawGraph; 
+
+  Vertex buildRoot(size_t root, size_t level = 0);
 };
